b879bc8d4e34da16e5ffedec5c6bfe1ea19604d1
kunal <knlgpt@gmail.com>
2025-06-21 16:08:26 +0530

    export update

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ana_trackPageView = exports.ana_trackEvent = void 0;
+exports.initAnalytics = exports.ana_trackPageView = exports.ana_trackEvent = void 0;
 var analytics_1 = require("./analytics");
 Object.defineProperty(exports, "ana_trackEvent", { enumerable: true, get: function () { return analytics_1.ana_trackEvent; } });
 Object.defineProperty(exports, "ana_trackPageView", { enumerable: true, get: function () { return analytics_1.ana_trackPageView; } });
+var mixpanel_1 = require("./mixpanel");
+Object.defineProperty(exports, "initAnalytics", { enumerable: true, get: function () { return mixpanel_1.initAnalytics; } });
--- index.ts
+++ index.ts
@@ -1,1 +1,2 @@
 export { ana_trackEvent, ana_trackPageView } from "./analytics";
+export { initAnalytics } from "./mixpanel";
